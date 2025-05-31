1f795f4e9a42b4b03fe98dee04a3b6f7e72dbf1f
Soren Furnell <soren.furnell@rcoa.ca>
2025-05-30 17:08:40 -0700

    added checkpoint system

--- game/camera_2d.gd
+++ game/camera_2d.gd
@@ -1,4 +1,5 @@
 extends Camera2D
+var room = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +10,11 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if get_parent().get_node("chara").position.y >= position.y+360:
 		position.y += 720
+		if room >= get_parent().get_node("Control").i:
+			get_parent().get_node("Control").i += 1
+		room += 1
 	if get_parent().get_node("chara").position.y <= position.y-360:
 		position.y -= 720
+		room -= 1
+		#get_parent().get_node("chara").die = true
+		#get_parent().get_node("Control").visible = true
--- game/respawn.gd
+++ game/respawn.gd
@@ -1,5 +1,7 @@
 extends Control
-
+var chara
+var checkPointPath
+@export var i = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +11,34 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	chara = get_parent().get_node("chara")
+	checkPointPath = get_parent().get_node("checkpoints/" + str(i)).position
 	position = get_parent().get_node("Camera2D").position
+	print(i)
 	if visible == true:
 		if get_node("Button").button_pressed == true || Input.is_action_pressed("ui_accept"):
+			if chara.fliped == true:
+				chara.velocity.y *= -1
+				chara.gravity *= -1
+				chara.jump_speed *= -1
+				chara.fliped = false
+				
 			visible = false
-			get_parent().get_node("chara").position = Vector2(-500, 100)
-			get_parent().get_node("chara").die = false
-			get_parent().get_node("chara").visible = true
+			chara.die = false
+			chara.visible = true
+			
+			match i:
+				4:
+					get_parent().get_node("Camera2D").position.y -= 720
+					get_parent().get_node("Camera2D").room -= 1
+					chara.position = checkPointPath
+					#chara.position = Vector2(458, 2300)
+#				3:
+					#chara.position = Vector2(458, 2300)
+#				2:
+					#chara.position = Vector2(564, 1432)
+#				1:
+					#chara.position = Vector2(400, 500)
+				_:
+					chara.position = checkPointPath
+					#chara.position = Vector2(-500, 100)
